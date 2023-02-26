5ec094189fd95d66feb0d0684eb77346a0be70d8
Kucheriavenko Dmitrii <Kucherdimdimych@mail.ru>
2023-02-26 23:30:14 +0500

    add flower

--- service/service/settings.py
+++ service/service/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 LOGGING = {
     'version': 1,
     'handlers': {
