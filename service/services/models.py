197b3d86df97e13d132539892a5f129551cfd87a
Kucheriavenko Dmitrii <Kucherdimdimych@mail.ru>
2023-03-03 20:06:39 +0500

    add django-cachalot

--- service/services/models.py
+++ service/services/models.py
@@ -56,9 +56,15 @@ class Subscription(models.Model):
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
-    comment = models.CharField(max_length=50, default='')
-
+    comment = models.CharField(max_length=50, default='', db_index=True)
+    field_a = models.CharField(max_length=50, default='')
+    field_b = models.CharField(max_length=50, default='')
     objects = models.Manager()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['field_a', 'field_b'])
+        ]
 
     def __str__(self):
         return f'{self.client} - {self.service} - {self.plan}'
@@ -66,8 +72,8 @@ class Subscription(models.Model):
     def save(self, *args, **kwargs):
         creating = not bool(self.id)
         result = super().save(*args, **kwargs)
-        if creating:
-            set_price.delay(self.id)
+        # if creating:
+        #     set_price.delay(self.id)
         return result
 
 
